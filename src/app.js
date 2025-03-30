bd9f2eead7e923859cd1a59e0ef507d27aabc13b
Nico <nico.nap@outlook.com>
2025-03-30 07:37:06 +0200

    feat: add session management

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
+require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { prisma } = require('./db/client');
+
 const createPhotoRouter = require('./routers/photo.js');
 const createLeaderboardRouter = require('./routers/leaderboard.js');
 
@@ -6,6 +11,21 @@ function createApp(database) {
   const app = express();
 
   app.use(express.json());
+  app.use(
+    session({
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      },
+      secret: process.env.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      store: new PrismaSessionStore(prisma, {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }),
+    })
+  );
 
   app.use('/photo', createPhotoRouter(database));
   app.use('/leaderboard', createLeaderboardRouter(database));
